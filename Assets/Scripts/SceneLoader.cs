45537994042855fb5b3ae1c801bde0047ba1dcbf
Hieu Ngo <justacpulover@gmail.com>
2020-12-20 22:46:04 +0100

    Add UI and scene loader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Awake() {
+        if (FindObjectsOfType<SceneLoader>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void HandleDropdown(int option){
+        switch (option)
+        {
+            default:
+                SceneManager.LoadScene(option);
+                break;
+        }
+    }
+}
