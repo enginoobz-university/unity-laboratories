5fd860d1581938ed2be685e1b2cf69434b0c7798
Hieu Ngo <justacpulover@gmail.com>
2020-12-26 11:20:10 +0100

    Add controller for cursor lock

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnlockMouse();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ToggleMouseLock();
+        }
+    }
+
+    private void ToggleMouseLock()
+    {
+        if (Cursor.lockState == CursorLockMode.None)
+        {
+            LockMouse();
+        }
+        else
+        {
+            UnlockMouse();
+        }
+    }
+
+    private void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void LockMouse()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
