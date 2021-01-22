50871e1a7ae9646d6b2e8a3e59ba82e39099a9ff
Hieu Ngo <justacpulover@gmail.com>
2021-01-22 13:39:54 +0100

    Design ball player & maze

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] float speed = 5;
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, verticalInput * speed * Time.deltaTime);
+    }
+}
